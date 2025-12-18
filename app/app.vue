c60e71bf0b38840dca2394171a5cf1363da968b8
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-12-18 15:39:33 +0100

    feat: wrap root components in ClientOnly with a fallback UI

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
 <template>
-  <AppShell>
-    <NuxtPage />
-  </AppShell>
+  <ClientOnly>
+    <AppShell>
+      <NuxtPage />
+    </AppShell>
+    <template #fallback>
+      <div class="min-h-screen bg-gray-50 dark:bg-gray-900" />
+    </template>
+  </ClientOnly>
 </template>
 <script setup lang="ts">
 import AppShell from '~/components/layout/AppShell.vue'
