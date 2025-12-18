edadf318da49dccf9e433a0d549c79a16aa69582
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-12-18 20:16:28 +0100

    feat: Add output for ACM certificate validation records

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -16,3 +16,14 @@ output "cloudfront_domain_name" {
   value       = try(aws_cloudfront_distribution.static_site.domain_name, null)
   description = "CloudFront domain name for static site"
 }
+
+output "acm_certificate_validation_records" {
+  value = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+  description = "DNS validation records for the ACM certificate"
+}
